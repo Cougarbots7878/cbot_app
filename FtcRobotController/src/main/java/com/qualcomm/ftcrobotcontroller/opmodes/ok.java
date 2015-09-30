58acd938d0e8b8202bde1c7e99ad678532cf6c6a
kai <seer@airmail.cc>
2015-09-30 14:38:42 -0700

    test commit, 2!

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ok.java
@@ -0,0 +1,21 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class ok extends OpMode {
+    DcMotor motor1;
+    DcMotor motor2;
+    @Override
+    public void init() {
+        motor1 = hardwareMap.dcMotor.get("motor_1");
+        motor2 = hardwareMap.dcMotor.get("motor_2");
+    }
+
+    @Override
+    public void loop() {
+        motor1.setPower(1.0);
+        motor2.setPower(1.0);
+
+    }
+}
